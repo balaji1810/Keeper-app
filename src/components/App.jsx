f1b71fb0d22aa2056561d952f8bc45fbcdf0b089
Balaji S B <balajibavimane@gmail.com>
2021-10-19 19:52:45 +0530

    Keeper app version-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note"
+import Note from "./Note";
+import notes from "../notes.js";
 
 function App() {
   return (
   <div>
     <Header />
-    <Note />
+    {notes.map( note =>{
+      return (
+        <Note
+          key={note.key}
+          title={note.title}
+          content={note.content}
+        />
+      );
+    } )}
     <Footer />
   </div>
   );
